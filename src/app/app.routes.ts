af540dfcdc252caa0ca56216e890f75fe8575834
Tu Nombre <luis.orihuela@upeu.edu.pe>
2025-08-17 18:58:09 -0500

    arreglando la ruta inicial para el despliegue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-    {
-        path: 'home',
-        loadComponent: () => import('./business/langindpage/langindpage.component').then(m => m.LangindpageComponent)
-    },
-    {
+   {
         path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
+        loadComponent: () => import('./business/langindpage/langindpage.component')
+            .then(m => m.LangindpageComponent)
     },
     {
         path: '**',
-        redirectTo: 'home'
+        redirectTo: ''
     }
 ];
